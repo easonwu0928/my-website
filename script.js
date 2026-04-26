8c9132623b267e2f65d6c6c111b0f3580be2e491
easonwu0928 <easonuwu960928@gmail.com>
2026-04-27 01:57:41 +0800

    update my title

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const myButton = document.getElementById('btn');
 // 當按鈕被點擊時，執行裡面的動作
 myButton.addEventListener('click', function() {
     // 彈出對話框
-    alert("魔法觸發成功！Eason，你已經學會連接 HTML、CSS 和 JS 了！");
+    alert("魔法觸發成功！");
     
     // 順便把網頁背景顏色換掉
     document.body.style.background = "#222";
